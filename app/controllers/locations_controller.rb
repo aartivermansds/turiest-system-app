61064d6ac9acbbafff3b536cdfa8f845c675d08d
aartivermansds <vermaaarti1996@gmail.com>
2017-08-21 09:12:09 +0530

    working on cocoon

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -69,6 +69,6 @@ class LocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-      params.require(:location).permit(:name, :zipcode, :city, :state, :about)
+      params.require(:location).permit(:name, :zipcode, :city, :state, :about, images_attributes: [:id,:image,:imageable_type,:imageable_id,:_destroy])
     end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
-    has_many :images, as: :imageable
+    has_many :images, as: :imageable, dependent: :destroy
+    accepts_nested_attributes_for :images, reject_if: :all_blank, allow_destroy: true
 end
