4de9037cb1ce7a60b2f36601035dad8b4b235d61
aartivermansds <vermaaarti1996@gmail.com>
2017-08-21 08:31:38 +0530

    added bootstrap

--- db/migrate/20170821024050_create_locations.rb
+++ db/migrate/20170821024050_create_locations.rb
@@ -1,6 +1,11 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
+        t.string :name
+        t.string :zipcode
+        t.string :city
+        t.string :state
+        t.text   :about
 
       t.timestamps null: false
     end
